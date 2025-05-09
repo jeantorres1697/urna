f7b0b3126c401679ef803d2a979e71fef6134952
Jean Torres <88167764+jeantorres1697@users.noreply.github.com>
2025-05-08 21:26:16 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,62 @@
+let etapas = [
+    {
+            titulo: 'VEREADOR',
+            numeros: 5,
+            candidatos: [
+                {
+                    numero: '11111',
+                    name: 'Michael Jackson',
+                    partido: 'Rusber',
+                    fotos: [
+                        {url:'/michael.png', legenda: 'Vereador'}
+                    ]
+                },
+
+                {
+                    numero: '24680',
+                    name: 'Saitama',
+                    partido: 'Super Herois',
+                    fotos: [
+                        {url: '/saitama.png', legenda: 'Vereador'}
+                    ]
+                },
+
+                {
+                    numero: '12345',
+                    name: 'Goku',
+                    partido: 'Sayajins',
+                    fotos: [
+                        {url: '/goku.png', legenda: 'Vereador'}
+                    ]
+                },
+            ]
+    },
+    
+    {
+            titulo: 'PREFEITO',
+            numeros: 2,
+            candidatos: [
+                {
+                    numero: '10',
+                    name: 'Seu Sirigueijo',
+                    partido: 'Siri Cascudo',
+                    vice: 'Patrick Estrela',
+                    fotos: [
+                        {url:'/siriguejo.png', legenda: 'Prefeito'},
+                        {url: '/patrick.png', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+                {
+                    numero: '20',
+                    name: 'Leo Stronda',
+                    partido: 'PVM - Partido Vira Monstro',
+                    vice: 'Bitelo',
+                    fotos: [
+                        {url:'Leostronda.png', legenda: 'Prefeito'},
+                        {url:'bitelo.png', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+            ]
+
+    }
+];
